b60b7f16de3432b4282afb28a973297c47992be8
anasred <anasahmar@gmail.com>
2020-01-14 11:28:12 +0300

    add test code to find out why I cannot download the pdf with annotations on it

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -1,30 +1,48 @@
 <template>
-  <div ref='viewer'></div>
+  <div ref="viewer"></div>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
-  name: 'WebViewer',
+  name: "WebViewer",
   props: {
     path: String,
     url: String
   },
-  mounted: function () {
-        WebViewer({
-            path: this.path,
-            initialDoc: this.url, // replace with your own PDF file
-          }, this.$refs.viewer).then((instance) => {
-            // call apis here
-          });
-    }
-}
+  mounted: function() {
+    WebViewer(
+      {
+        path: this.path,
+        initialDoc: this.url // replace with your own PDF file
+      },
+      this.$refs.viewer
+    ).then(instance => {
+      // call apis here
+      const { docViewer, annotManager } = instance;
+
+      docViewer.on("documentLoaded", async () => {
+        const doc = docViewer.getDocument();
+        const xfdfString = await annotManager.exportAnnotations();
+        const options = { xfdfString };
+        const data = await doc.getFileData(options);
+        const arr = new Uint8Array(data);
+        const blob = new Blob([arr], { type: "application/pdf" });
+
+        setTimeout(() => {
+          console.log('xfdfString', xfdfString);
+          window.saveAs(blob, "downloaded.pdf");
+        }, 15000);
+      });
+    });
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 div {
-  width: 100%; 
+  width: 100%;
   height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+window.saveAs = require('file-saver');
 
 Vue.config.productionTip = false
 
